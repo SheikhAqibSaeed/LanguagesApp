680545d334177f4acfb8f6c716d61da5442fb10c
Aqib <aqibsaeed570@gmail.com>
2023-07-17 00:14:05 +0500

    Added in NumbersActivity and write logic. Added in number xml file and set the rootView. In NumbersActivity added the ArrayList and define. Added different methods in NumbersActivity. Using While loop. Simple method. For Loop.

--- app/src/main/java/com/example/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/example/miwokapp/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.miwokapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,51 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Saven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
+
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        //3rd method
+        for (int index=0; index<words.size(); index++){
+            TextView wordsView1 = new TextView(this);
+            wordsView1.setText(words.get(index));
+            rootView.addView(wordsView1);
+        }
+
+//        int index = 0;
+//
+//        // Executing for 2nd method
+//        while (index<words.size()){
+//            TextView wordsView1 = new TextView(this);
+//            wordsView1.setText(words.get(index));
+//            rootView.addView(wordsView1);
+//            index++;
+//        }
+        // Executing for 1s method
+//        TextView wordsView1 = new TextView(this);
+//        wordsView1.setText(words.get(index));
+//        rootView.addView(wordsView1);
+//        index++;
+//
+//        TextView wordsView2 = new TextView(this);
+//        wordsView2.setText(words.get(index));
+//        rootView.addView(wordsView2);
+//        index++;
+//
+//        TextView wordsView3 = new TextView(this);
+//        wordsView3.setText(words.get(index));
+//        rootView.addView(wordsView3);
+//        index++;
     }
 }
