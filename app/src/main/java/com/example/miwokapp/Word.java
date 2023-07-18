15086ea20bc2447937387fdae958fd83cdd3e547
Aqib <aqibsaeed570@gmail.com>
2023-07-18 10:43:56 +0500

    Added Image constructor and method overloading. Added Image Visibility method.

--- app/src/main/java/com/example/miwokapp/Word.java
+++ app/src/main/java/com/example/miwokapp/Word.java
@@ -4,10 +4,18 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mUrduTranslation;
+    private int mImagesResource =ON_IMAGE_PROVIDER;
+
+    private static final int ON_IMAGE_PROVIDER=-1;
 
     public Word(String DefaultTranslation, String UrduTranslation){
         mDefaultTranslation = DefaultTranslation;
         mUrduTranslation = UrduTranslation;
+    }
+    public Word(String DefaultTranslation, String UrduTranslation, int ImageReource){
+        mDefaultTranslation = DefaultTranslation;
+        mUrduTranslation = UrduTranslation;
+        mImagesResource = ImageReource;
     }
 
     public String getDefaultTranslation(){
@@ -15,5 +23,12 @@ public class Word {
     }
     public String getUrduTranslation(){
         return mUrduTranslation;
+    }
+    public int getImagesResource()
+    {
+        return mImagesResource;
+    }
+    public boolean hasImage(){
+        return mImagesResource != ON_IMAGE_PROVIDER;
     }
 }
