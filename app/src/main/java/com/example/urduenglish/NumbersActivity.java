6e1ab79a3607165bd13b496e29f5473e66eaa38d
Aqib <aqibsaeed570@gmail.com>
2023-07-20 10:40:27 +0500

    App Life Cycle States Added OnStop Method in Activity files Added and applying Audio Focus. Added and Creating Instance. Added Abandon Audio Focus.

--- app/src/main/java/com/example/urduenglish/NumbersActivity.java
+++ app/src/main/java/com/example/urduenglish/NumbersActivity.java
@@ -2,7 +2,10 @@ package com.example.urduenglish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.RemoteController;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,29 +17,50 @@ import java.util.ArrayList;
 public class NumbersActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
+    private AudioManager mAudioManager;
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK) {
+                //pause playback
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                //resume playback
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                //stop playback
+                mMediaPlayer.stop();
+            }
+        }
+    };
     private MediaPlayer.OnCompletionListener mComplitionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
             releaseMediaPlayer();
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.word_list);
 
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         final ArrayList<Word> words = new ArrayList<Word>();
 
-        words.add(new Word("One",  "ایک",R.drawable.number_one, R.raw.one));
-        words.add(new Word("Two", "دو",R.drawable.number_two,R.raw.two));
-        words.add(new Word("Three", "تین",R.drawable.number_three, R.raw.three));
-        words.add(new Word("Four", "چار",R.drawable.number_four, R.raw.four));
-        words.add(new Word("Five", "پانچ",R.drawable.number_five, R.raw.five));
-        words.add(new Word("Six", "چھ",R.drawable.number_six, R.raw.six));
-        words.add(new Word("Seven", "سات",R.drawable.number_seven, R.raw.seven));
-        words.add(new Word("Eight", "آٹھ",R.drawable.number_eight, R.raw.eight));
-        words.add(new Word("Nine", "نو",R.drawable.number_nine, R.raw.nine));
-        words.add(new Word("Ten", "دس",R.drawable.number_ten, R.raw.ten));
+        words.add(new Word("One", "ایک", R.drawable.number_one, R.raw.one));
+        words.add(new Word("Two", "دو", R.drawable.number_two, R.raw.two));
+        words.add(new Word("Three", "تین", R.drawable.number_three, R.raw.three));
+        words.add(new Word("Four", "چار", R.drawable.number_four, R.raw.four));
+        words.add(new Word("Five", "پانچ", R.drawable.number_five, R.raw.five));
+        words.add(new Word("Six", "چھ", R.drawable.number_six, R.raw.six));
+        words.add(new Word("Seven", "سات", R.drawable.number_seven, R.raw.seven));
+        words.add(new Word("Eight", "آٹھ", R.drawable.number_eight, R.raw.eight));
+        words.add(new Word("Nine", "نو", R.drawable.number_nine, R.raw.nine));
+        words.add(new Word("Ten", "دس", R.drawable.number_ten, R.raw.ten));
 
 
         //LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
@@ -50,12 +74,19 @@ public class NumbersActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Word word = words.get(position);
+
                 releaseMediaPlayer();
 
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudtioResource());
-                mMediaPlayer.start();
-                mMediaPlayer.setOnCompletionListener(mComplitionListener);
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudtioResource());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mComplitionListener);
 //                Toast.makeText(NumbersActivity.this, "List Item Clicked", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -93,14 +124,16 @@ public class NumbersActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         releaseMediaPlayer();
     }
-    private void releaseMediaPlayer(){
-        if(mMediaPlayer!=null){
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
             mMediaPlayer.release();
             mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
         }
     }
 }
